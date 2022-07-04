cdc5f6fdb03c290ca43c40fe6f4a30f65e9b340a
雷义焘Abner <73002944+ABNER0216@users.noreply.github.com>
2022-07-04 17:35:13 +0800

    👴做完了

--- Project_group/Project_group/CheckOut.aspx.cs
+++ Project_group/Project_group/CheckOut.aspx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -51,15 +52,47 @@ namespace Project_group
         {
 
         }
-
+        public bool checkout=true;
         protected void SubmitOrder_Click(object sender, EventArgs e)
         {
             decimal totalp = Decimal.Parse(totalprice.Text);
-            
-            string sql = string.Format("insert into orderinfo(OrderUser, Price,  OrderAddress, OrderDate, Realname, ContactNo) values('{0}','{1}','{2}','{3}','{4}','{5}');", Session["UserName"].ToString(), totalp, TextBox2.Text.ToString(), DateTime.Now, TextBox1.Text.ToString(), Convert.ToInt32(TextBox3.Text.ToString()));
-            DBHelper.GetExecute(sql);
-            complete.Visible = true;   
-            content.Visible = false;   
+            string checkinventory = "UPDATE ShoppingCart set remain=Inventory-ItemNum";
+            DBHelper.GetExecute(checkinventory);
+            var list = new List<int>();
+            string connection = @"Data Source=雷义焘\SQLEXPRESS01;initial catalog=community; integrated security=true";
+            SqlConnection con = new SqlConnection(connection);
+            con.Open();
+            string query = "select remain from ShoppingCart";
+            SqlCommand cmd = new SqlCommand(query);
+            cmd.Connection = con;
+            cmd.CommandType = CommandType.Text;
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                list.Add(reader.GetInt32(0));
+            }
+            foreach(int data in list)
+            {
+                if (data < 0)
+                {
+                    checkout = false;
+                }
+            }
+            if (checkout == true)
+            {
+                string updatesql = "update ItemList set ItemList.Inventory = ShoppingCart.remain from ShoppingCart where ItemList.ItemID=ShoppingCart.ItemId";
+                string updatesql2 = "update ShoppingCart set ShoppingCart.Inventory = ShoppingCart.remain from ShoppingCart";
+                DBHelper.GetExecute(updatesql);
+                DBHelper.GetExecute(updatesql2);
+                string sql = string.Format("insert into orderinfo(OrderUser, Price,  OrderAddress, OrderDate, Realname, ContactNo) values('{0}','{1}','{2}','{3}','{4}','{5}');", Session["UserName"].ToString(), totalp, TextBox2.Text.ToString(), DateTime.Now, TextBox1.Text.ToString(), Convert.ToInt32(TextBox3.Text.ToString()));
+                DBHelper.GetExecute(sql);
+                complete.Visible = true;
+                content.Visible = false;
+            }
+            else
+            {
+                Response.Write("<script>window.alert('There is no enough inventory, please try again！');</script>");
+            }
 
         }
     }
